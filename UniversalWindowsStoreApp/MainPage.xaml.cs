5b14ce9aeb7078c25e1df6ab2e67bacf90391cf6
truongnnd00546 <truongnnd00546@fpt.edu.vn>
2018-11-01 10:11:02 +0700

    commit-success

---
+++ UniversalWindowsStoreApp/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text.RegularExpressions;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UniversalWindowsStoreApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            collection = new ObservableCollection<RootObject>();
+            this.DataContext = this;
+        }
+        public ObservableCollection<RootObject> collection { get; set; }
+        private static string RemoveHtmlTag(string value)
+        {
+            var step1 = Regex.Replace(value, @"<[^>]+>|&nbsp;", "").Trim();
+            var step2 = Regex.Replace(step1, @"\s{2,}", " ");
+            return step2;
+        }
+
+        private async void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            List<RootObject> data = await POJO.GetData();
+
+            for (int i = 0; i < data.Count; i++)
+            {
+                collection.Add(data[i]);
+            }
+            APIGridView.ItemsSource = collection;
+        }
+    }
+}
---
+++ UniversalWindowsStoreApp/POJO.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace UniversalWindowsStoreApp
+{
+    class POJO
+    {
+        public static async Task<List<RootObject>> GetData()
+        {
+            var http = new HttpClient();
+            var url = String.Format("http://youtube-video-api-1608.appspot.com/youtube/api");
+            var response = await http.GetAsync(url);
+            var result = await response.Content.ReadAsStringAsync();
+            List<RootObject> data = JsonConvert.DeserializeObject<List<RootObject>>(result);
+            return data;
+        }
+    }
+    public class RootObject
+    {
+        public string videoId { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public string keywords { get; set; }
+        public string category { get; set; }
+        public string genre { get; set; }
+        public string authorName { get; set; }
+        public string authorEmail { get; set; }
+        public string birthday { get; set; }
+    }
+}
